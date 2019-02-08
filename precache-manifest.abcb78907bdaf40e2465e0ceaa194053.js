3e5eda2603a0890e9aa051eefe8b1e0abcc7bfc0
John Doe <john@doe.org>
2019-02-08 17:07:50 +0100

    init

---
+++ precache-manifest.abcb78907bdaf40e2465e0ceaa194053.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "6f0715220a6472ddd734",
+    "url": "./static/js/main.6f071522.chunk.js"
+  },
+  {
+    "revision": "c46ce70d310d450933a9",
+    "url": "./static/js/1.c46ce70d.chunk.js"
+  },
+  {
+    "revision": "6f0715220a6472ddd734",
+    "url": "./static/css/main.f6ff44ad.chunk.css"
+  },
+  {
+    "revision": "777a2f68813051a66f5dc8c68debf51d",
+    "url": "./index.html"
+  }
+];
